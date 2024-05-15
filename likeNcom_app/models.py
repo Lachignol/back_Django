e27da5c6c08d2b1d1c72d1b8220e647a756ede0a
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-05-15 18:54:14 +0200

    avancar

--- likeNcom_app/models.py
+++ likeNcom_app/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from auth_app.models import User
-# from food_app.models import Restaurant
 # Create your models here.
 
 
 class Like(models.Model):
     user= models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
-    # restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
      
      
     def __str__(self):
@@ -16,7 +14,6 @@ class Like(models.Model):
     
 class Comment(models.Model):
     user= models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
-    # restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
     content= models.TextField(max_length=255)
      
      
--- likeNcom_app/views.py
+++ likeNcom_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 from food_app.models import Restaurant
@@ -10,11 +11,13 @@ from .models import *
 
 
 def addOrDeleteLike(request,restaurantId):
-
-    restaurant = Restaurant.objects.get(pk=restaurantId)
-    print(restaurant.likes)
-    # if like := restaurant.likes.user == user:
-    #     like.delete()
-    # else : 
-    restaurant.likes.add(Like.objects.create(user=request.user))
-    return HttpResponseRedirect("one_restaurant")  
+    if request.method == "POST" :
+        restaurant = Restaurant.objects.get(pk=restaurantId)
+        print("tchikec",restaurant.likes.filter(user=request.user))
+        if like := restaurant.likes.filter(user=request.user):
+            like.delete()
+        else : 
+            restaurant.likes.add(Like.objects.create(user=request.user))
+        return HttpResponseRedirect(reverse("food_app:one_restaurant", kwargs={"restaurantId":restaurantId}))
+    else :
+        return HttpResponseRedirect(reverse("food_app:one_restaurant",kwargs={"restaurantId":restaurantId}))  
