fff164bd3094d692385d5635e2463e8971696b47
Votre Nom <votre_email@exemple.com>
2025-08-17 03:43:45 +0200

    add dockerfile and cmd for create superuser infile cmd.txt

--- project_test/settings.py
+++ project_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)=d(ef&lgb106_339=@7q%i&)q1-#2@y7ed@omro-^-g8mp2k*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
